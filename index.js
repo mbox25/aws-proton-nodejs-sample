3b31202f0defd83eddd9571af7e5185fa7bb2cde
Daniel <mbox2u@hotmail.com>
2021-04-27 20:10:07 +0900

    test

--- index.js
+++ index.js
@@ -6,4 +6,8 @@ app.get("/", (req, res) =>
   res.send({ status: "✅ success", message: "proton is awesome ❤️" })
 );
 
+app.get("/test1", (req, res) =>
+  res.send({ status: "success", message: "this is just test!!!" })
+);
+
 app.listen(80, () => console.log("server listening on 80"));
