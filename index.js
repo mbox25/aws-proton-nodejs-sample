b06e5c3a36616214269d4814e8dad46644f1bdc2
Daniel <mbox2u@hotmail.com>
2021-05-06 11:25:34 +0900

    test

--- index.js
+++ index.js
@@ -1,13 +1,10 @@
 const express = require("express");
-
 const app = new express();
 
-app.get("/test1", (req, res) =>
-  res.send({ status: "success", message: "테스트" })
-);
 
 app.get("/", (req, res) =>
   res.send({ status: "success", message: "✅ AWS Proton" })
 );
+
 
 app.listen(80, () => console.log("server listening on 80"));
